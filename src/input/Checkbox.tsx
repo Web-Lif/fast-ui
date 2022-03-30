69bf37365dc893f85d493acb6adf5cfc41a7c614
robothot <854363956@qq.com>
2022-03-30 09:25:40 +0800

    feat: 修复 Checkbox 不能输入的问题

--- src/input/Checkbox.tsx
+++ src/input/Checkbox.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useMemo } from 'react';
 import { Checkbox as AntCheckbox, CheckboxProps as AntCheckboxProps } from 'antd';
 import { CheckboxChangeEvent } from 'antd/es/checkbox';
 
@@ -8,13 +8,23 @@ interface CheckboxProps extends Omit<AntCheckboxProps, 'value' | 'checked' | 'on
 }
 
 const InternalCheckbox: FC<CheckboxProps> = ({ value, onChange, ...restProps }) => {
+    const props = useMemo(() => {
+        if (value) {
+            return {
+                checked: value,
+                ...restProps,
+            }
+        }
+        return restProps
+    }, [value])
+
     return (
         <AntCheckbox
             onChange={(e) => {
                 onChange?.(e.target.checked, e);
             }}
-            checked={value}
-            {...restProps}
+
+            {...props}
         />
     );
 };
