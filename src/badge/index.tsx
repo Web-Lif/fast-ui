f9e5cc0e086ca038f1511c960c6401b15b3ca8dc
robothot <854363956@qq.com>
2022-04-14 10:42:56 +0800

    fix: 修复 react 18 中的类型定义问题

--- src/badge/index.tsx
+++ src/badge/index.tsx
@@ -1,1 +1,12 @@
-export { Badge } from 'antd'
+import React, { ReactNode } from 'react'
+import { Badge as AntBadge, BadgeProps as AntBadgeProps } from 'antd'
+
+interface BadgeProps extends AntBadgeProps {
+    children?: ReactNode
+}
+
+const Badge = (props: BadgeProps) => {
+    return <AntBadge {...props}/>
+}
+
+export default Badge
--- src/dropdown/Dropdown.tsx
+++ src/dropdown/Dropdown.tsx
@@ -1,1 +1,21 @@
-export { Dropdown } from 'antd'
+import React, { ReactNode } from 'react';
+import { Dropdown as AntDropdown, DropDownProps  } from 'antd'
+
+interface InternalDropdownProps extends DropDownProps {
+    children?: ReactNode
+}
+
+const InternalDropdown = (props: InternalDropdownProps) => {
+    return <AntDropdown {...props}/>
+}
+
+type InternalDropdownType = typeof InternalDropdown;
+
+interface DropdownInterface extends InternalDropdownType {
+    Button: typeof AntDropdown.Button
+}
+
+const Dropdown = InternalDropdown as DropdownInterface;
+Dropdown.Button = AntDropdown.Button
+
+export default Dropdown
