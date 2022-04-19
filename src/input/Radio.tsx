6c87b1aa27946f331099f83865e906f774f35880
robothot <854363956@qq.com>
2022-04-19 18:10:14 +0800

    fix: 修复 radio 组件的问题

--- src/input/Radio.tsx
+++ src/input/Radio.tsx
@@ -1,15 +1,22 @@
-import React, { FC } from 'react'
-import { Radio as AntRadio, RadioProps as AntRadioProps } from 'antd'
+import React, { FC } from 'react';
+import { Radio as AntRadio, RadioProps as AntRadioProps } from 'antd';
 
-interface RadioProps extends AntRadioProps {
+interface RadioProps extends AntRadioProps {}
+
+const InternalRadio: FC<RadioProps> = ({ ...restProps }) => {
+    return <AntRadio {...restProps} />;
+};
+
+type InternalRadioType = typeof InternalRadio;
+
+interface RadioInterface extends InternalRadioType {
+    Button: typeof AntRadio.Button;
+    Group: typeof AntRadio.Group;
 }
 
-const Radio: FC<RadioProps> = ({
-    ...restProps
-}) => {
-    return (
-        <AntRadio {...restProps}/>
-    )
-}
+const Radio = InternalRadio as RadioInterface;
 
-export default Radio
+Radio.Button = AntRadio.Button;
+Radio.Group = AntRadio.Group;
+
+export default Radio;
