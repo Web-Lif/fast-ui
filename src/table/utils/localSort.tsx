7dfee6a19950115faed24dc5bd0682592bf9c078
robothot <854363956@qq.com>
2022-05-25 15:02:45 +0800

    fix: 修复选中表格行会导致触发排序的问题

--- src/table/utils/localSort.tsx
+++ src/table/utils/localSort.tsx
@@ -16,39 +16,49 @@ function useTableLocalSort<T>(data: T[] = []) {
         );
     }, []);
 
-    const sortRows = () => {
-        const directionElement = sortDirection.find((element) => element.direction);
-        if (directionElement?.name) {
-            return produce(rows, (draft) => {
-                draft.sort((element: any, nextElement: any) => {
-                    if (directionElement.direction === 'ASC') {
-                        if (element[directionElement.name] > nextElement[directionElement.name]) {
-                            return 1;
-                        }
-                        return -1;
-                    } else {
-                        if (element[directionElement.name] < nextElement[directionElement.name]) {
-                            return 1;
-                        }
-                        return -1;
-                    }
-                });
-            });
-        }
-        return produce(rows, (draft) => {
-            draft.sort((element: any, nextElement: any) => {
-                if (element.$$serialNumber > nextElement.$$serialNumber) {
-                    return 1;
-                }
-                return -1;
-            });
-        });
-    };
-
     return {
         sortDirection,
-        setSortDirection,
-        rows: sortRows(),
+        setSortDirection: (sortDirection: SortDirection[]) => {
+            const directionElement = sortDirection.find((element) => element.direction);
+            setSortDirection(sortDirection);
+            if (directionElement?.name) {
+                setRows(
+                    produce(rows, (draft) => {
+                        draft.sort((element: any, nextElement: any) => {
+                            if (directionElement.direction === 'ASC') {
+                                if (
+                                    element[directionElement.name] >
+                                    nextElement[directionElement.name]
+                                ) {
+                                    return 1;
+                                }
+                                return -1;
+                            } else {
+                                if (
+                                    element[directionElement.name] <
+                                    nextElement[directionElement.name]
+                                ) {
+                                    return 1;
+                                }
+                                return -1;
+                            }
+                        });
+                    }),
+                );
+                return;
+            }
+            setRows(
+                produce(rows, (draft) => {
+                    draft.sort((element: any, nextElement: any) => {
+                        if (element.$$serialNumber > nextElement.$$serialNumber) {
+                            return 1;
+                        }
+                        return -1;
+                    });
+                }),
+            );
+        },
+        rows,
         setRows,
     };
 }
