030f8931fd64ee5cae5fda4ef0ac5774d5278376
苯苯 <91561865+robothot@users.noreply.github.com>
2022-06-09 16:27:12 +0800

    feat: Layout 更换到 react-spaces (#101)

---
+++ src/layout/__demo__/simple_layout.tsx
@@ -0,0 +1,44 @@
+/**
+ * title: 基础
+ * desc: 典型的页面布局。
+ */
+import { Layout } from '@weblif/fast-ui';
+import React from 'react';
+
+const Simple = () => (
+    <Layout.Fixed height={500}>
+        <Layout.Fill>
+            <Layout.Left
+                style={{
+                    backgroundColor: '#3ba0e9',
+                }}
+                size="20%"
+            />
+            <Layout.Fill
+                style={{
+                    backgroundColor: '#108ee9',
+                }}
+            />
+            <Layout.Right
+                style={{
+                    backgroundColor: '#3ba0e9',
+                }}
+                size="20%"
+            />
+        </Layout.Fill>
+        <Layout.Top
+            size={64}
+            style={{
+                backgroundColor: '#7dbcea',
+            }}
+        />
+        <Layout.Bottom
+            size={64}
+            style={{
+                backgroundColor: '#7dbcea',
+            }}
+        />
+    </Layout.Fixed>
+);
+
+export default Simple;
