0915d405c0e05300210a9c3658f8dfea18581144
robothot <854363956@qq.com>
2022-06-14 08:36:07 +0800

    feat: 添加虚拟滚动的 demo

---
+++ src/table/__demo__/virtualscroll.tsx
@@ -0,0 +1,105 @@
+/**
+ * title: 虚拟滚动
+ * desc: 1000条表格数据信息进行测试
+ */
+import { Table } from '@weblif/fast-ui';
+import React, { useState } from 'react';
+
+/** Mock 数据 */
+const mockData = () => {
+    return Array.from(
+        {
+            length: 1000,
+        },
+        (_, i) => ({
+            username: 'zhangj',
+            email: 'zhang@xx.com',
+            id: i,
+            age: i,
+            tags: '人类',
+            col0: `col0 - ${i}`,
+            col1: `col1 - ${i}`,
+            col2: `col2 - ${i}`,
+            col3: `col3 - ${i}`,
+            col4: `col4 - ${i}`,
+            col5: `col5 - ${i}`,
+            col6: `col6 - ${i}`,
+        }),
+    );
+};
+
+export default () => {
+    const [data, setData] = useState(mockData());
+    return (
+        <>
+            <Table
+                style={{
+                    height: 600,
+                    width: 1200,
+                }}
+                rowKey="id"
+                mode="cell"
+                columns={[
+                    {
+                        name: 'username',
+                        title: '人员名称',
+                        width: 120,
+                        fixed: 'right',
+                    },
+                    {
+                        name: 'email',
+                        width: 120,
+                        title: '邮箱地址',
+                    },
+                    {
+                        name: 'age',
+                        width: 120,
+                        title: '年龄',
+                    },
+                    {
+                        name: 'tags',
+                        width: 120,
+                        title: '标签',
+                    },
+                    {
+                        name: 'col0',
+                        width: 120,
+                        title: '标签',
+                    },
+                    {
+                        name: 'col1',
+                        width: 120,
+                        title: '标签',
+                    },
+                    {
+                        name: 'col2',
+                        title: '标签',
+                        width: 120,
+                    },
+                    {
+                        name: 'col3',
+                        title: '标签',
+                        width: 120,
+                    },
+                    {
+                        name: 'col4',
+                        title: '标签',
+                        width: 120,
+                    },
+                    {
+                        name: 'col5',
+                        title: '标签',
+                        width: 120,
+                    },
+                    {
+                        name: 'col6',
+                        title: '标签',
+                        width: 120,
+                    },
+                ]}
+                rows={data}
+                onChange={setData}
+            />
+        </>
+    );
+};
