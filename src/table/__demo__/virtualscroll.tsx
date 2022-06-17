6a969c8295a1fd0640428e0c715392d7cd482f1b
苯苯 <91561865+robothot@users.noreply.github.com>
2022-06-17 17:50:30 +0800

    Revert "docs: 表格的行数上升到1万条数据 (#147)"
    This reverts commit e3d89ae0e7d7b7979919cea3697d4789f9f89469.

--- src/table/__demo__/virtualscroll.tsx
+++ src/table/__demo__/virtualscroll.tsx
@@ -1,15 +1,15 @@
 /**
  * title: 虚拟滚动
- * desc: 1万条表格数据信息进行测试
+ * desc: 1000条表格数据信息进行测试
  */
 import { Table } from '@weblif/fast-ui'
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 
 /** Mock 数据 */
 const mockData = () => {
     return Array.from(
         {
-            length: 10000,
+            length: 1000,
         },
         (_, i) => ({
             username: 'zhangj',
@@ -29,7 +29,7 @@ const mockData = () => {
 }
 
 export default () => {
-    const [data, setData] = useState([])
+    const [data, setData] = useState(mockData())
     const [cols, setCols] = useState<any[]>([
         {
             name: 'username',
@@ -88,14 +88,6 @@ export default () => {
             width: 120,
         },
     ])
-    useEffect(() => {
-        setTimeout(() => {
-            setData(mockData())
-        }, 0)
-    }, [])
-    if (data.length === 0) {
-        return <> 正在加载数据, 一共加载1万条数据... </>
-    }
     return (
         <>
             <Table
