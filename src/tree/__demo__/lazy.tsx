ae9a58bbdb3b1f613336d848ce3bbf6c0a0e0f79
苯苯 <91561865+robothot@users.noreply.github.com>
2022-06-24 10:12:02 +0800

    feat: 优化 Tree 的加载效率, 默认情况下就会开启虚拟滚动 (#157)

--- src/tree/__demo__/lazy.tsx
+++ src/tree/__demo__/lazy.tsx
@@ -2,15 +2,16 @@
  * title: 懒加载
  * desc: 异步加载树的信息, 可极大的优化加载效率
  */
-import { Tree } from '@weblif/fast-ui';
-import React, { useRef, useState } from 'react';
+import { Tree } from '@weblif/fast-ui'
+import React, { useRef, useState } from 'react'
 
 export default () => {
-    const tree = useRef();
-    const [treeData, setTreeData] = useState([]);
+    const tree = useRef()
+    const [treeData, setTreeData] = useState([])
     return (
         <Tree
             tree={tree}
+            style={{ height: 250 }}
             treeData={treeData}
             onChangeTreeData={setTreeData}
             loadData={(treeNode) => {
@@ -20,35 +21,41 @@ export default () => {
                             { title: 'Expand to load', key: '0' },
                             { title: 'Expand to load', key: '1' },
                             { title: 'Tree Node', key: '2', isLeaf: true },
-                        ]);
-                        return;
+                        ])
+                        return
                     }
 
                     setTimeout(() => {
                         resolve([
-                            { title: `Child Node - ${treeNode.key}`, key: `${treeNode.key}-0` },
-                            { title: `Child Node - ${treeNode.key}-1`, key: `${treeNode.key}-1` },
-                        ]);
-                    }, 1000);
-                });
+                            {
+                                title: `Child Node - ${treeNode.key}`,
+                                key: `${treeNode.key}-0`,
+                            },
+                            {
+                                title: `Child Node - ${treeNode.key}-1`,
+                                key: `${treeNode.key}-1`,
+                            },
+                        ])
+                    }, 1000)
+                })
             }}
             contextMenuRender={(node) => {
                 if (node === null) {
-                    return [];
+                    return []
                 }
                 return [
                     {
                         key: 'delete',
                         label: '删除',
                         onClick: () => {
-                            tree.current.removeNodes([node.key]);
+                            tree.current.removeNodes([node.key])
                         },
                     },
                     {
                         key: 'refresh',
                         label: '刷新',
                         onClick: () => {
-                            tree.current.refresh(node);
+                            tree.current.refresh(node)
                         },
                     },
                     {
@@ -62,8 +69,8 @@ export default () => {
                                         title: `Add Child Node - ${node.key}`,
                                         key: `${node.key}-add-0`,
                                     },
-                                ];
-                            });
+                                ]
+                            })
                             tree.current.addNodes(node, (nodes) => {
                                 return [
                                     ...nodes,
@@ -71,8 +78,8 @@ export default () => {
                                         title: `Add Child Node - ${node.key} 1`,
                                         key: `${node.key}-add-1`,
                                     },
-                                ];
-                            });
+                                ]
+                            })
                         },
                     },
                     {
@@ -81,11 +88,11 @@ export default () => {
                         onClick: () => {
                             tree.current.editNode(node.key, {
                                 title: `${node.title} - 已修改`,
-                            });
+                            })
                         },
                     },
-                ];
+                ]
             }}
         />
-    );
-};
+    )
+}
