4c4b39971c34bcee6dbc15419b53405f15fa7792
robothot <854363956@qq.com>
2022-06-25 12:39:31 +0800

    test: 添加 Button 的测试用例

--- src/button/__tests__/button.test.tsx
+++ src/button/__tests__/button.test.tsx
@@ -1,34 +1,63 @@
-import React from 'react';
-import { render, fireEvent, waitFor, act } from '@testing-library/react';
-import Button from '../index';
+import React from 'react'
+import { render, fireEvent, waitFor, act } from '@testing-library/react'
+import Button from '../index'
 
 const sleep = (time: number) => {
     return new Promise<void>((resolve) => {
         setTimeout(() => {
-            resolve();
-        }, time);
-    });
-};
+            resolve()
+        }, time)
+    })
+}
 
-test('Button -> test fast double click', async () => {
-    const mockClickEvent = jest.fn();
+test('snapshot', async () => {
+    const { container } = render(<Button>button</Button>)
+    expect(container).toMatchSnapshot()
+})
+
+test('test disabled', async () => {
+    const { container } = render(<Button disabled>button</Button>)
+    expect(container).toMatchSnapshot()
+})
+
+test('test fast double click promise', async () => {
+    const mockClickEvent = jest.fn()
 
     const { getByText } = render(
         <Button
             onClick={async () => {
-                mockClickEvent();
-                await sleep(100);
+                mockClickEvent()
+                await sleep(100)
             }}
         >
             button
-        </Button>,
-    );
+        </Button>
+    )
 
-    const button = await waitFor(() => getByText('button'));
-    fireEvent.click(button);
-    fireEvent.click(button);
+    const button = await waitFor(() => getByText('button'))
+    fireEvent.click(button)
+    fireEvent.click(button)
     act(() => {
-        fireEvent.click(button);
-    });
-    expect(mockClickEvent.mock.calls.length).toBe(1);
-});
+        fireEvent.click(button)
+    })
+    expect(mockClickEvent.mock.calls.length).toBe(1)
+})
+
+test('test fast double click', async () => {
+    const mockClickEvent = jest.fn()
+    const { getByText } = render(
+        <Button
+            onClick={() => {
+                mockClickEvent()
+            }}
+        >
+            button
+        </Button>
+    )
+
+    const button = await waitFor(() => getByText('button'))
+    fireEvent.click(button)
+    setTimeout(() => {
+        expect(mockClickEvent.mock.calls.length).toBe(1)
+    }, 0)
+})
