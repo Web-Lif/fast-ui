1af672c2cbe8079e2f9cc286ac7fa4c966983a5b
苯苯 <91561865+robothot@users.noreply.github.com>
2022-07-12 10:20:08 +0800

    fix: 修复文档的国际化内容 (#184)
    * fix: 修复文档的国际化内容
    
    * feat: 添加对等依赖
    
    * feat: 在执行 CI 之前更新 npm 到最新的依赖
    
    * feat: 添加 overrides
    
    * fix: 添加 lock 文件
    
    * fix: 修复依赖问题
    
    * fix: 修复构建错误信息

--- src/calendar/index.tsx
+++ src/calendar/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { CalendarProps as AntCalendarProps } from 'antd/es/calendar'
 import generateCalendar from 'antd/es/calendar/generateCalendar'
-import zhCN from 'antd/lib/locale/zh_CN'
 import dayJSGenerateConfig from 'rc-picker/es/generate/dayjs'
 import { Dayjs } from 'dayjs'
 
@@ -12,9 +11,6 @@ interface CalendarProps extends AntCalendarProps<Dayjs> {}
 export type { CalendarProps }
 export { Calendar }
 
-const Calendar = ({
-    locale = zhCN.DatePicker,
-    ...restProps
-}: CalendarProps) => {
-    return <InternalCalendar locale={locale} {...restProps} />
+const Calendar = ({ ...restProps }: CalendarProps) => {
+    return <InternalCalendar {...restProps} />
 }
--- src/pagination/index.tsx
+++ src/pagination/index.tsx
@@ -3,12 +3,10 @@ import AntPagination, {
     PaginationProps as AntPaginationProps,
 } from 'antd/es/pagination'
 
-import zhCN from 'antd/lib/locale/zh_CN'
-
 export interface PaginationProps extends AntPaginationProps {}
 
 const Pagination: FC<PaginationProps> = ({ ...restProps }) => {
-    return <AntPagination locale={zhCN.Pagination} {...restProps} />
+    return <AntPagination {...restProps} />
 }
 
 export default Pagination
