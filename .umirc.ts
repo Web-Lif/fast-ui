de5bd6fbaccf32cc728ce1c361d8c7b541b481ca
苯苯 <91561865+robothot@users.noreply.github.com>
2022-07-19 09:45:07 +0800

    fix: 修复没有显示表格总数的问题 (#196)
    * fix: 修复没有显示表格总数的问题
    
    * fix: 修复 chunk 的问题
    
    * fix: 修复开发环境下样式丢失的问题
    
    * fix: 修复开发环境下样式丢失的问题

--- .umirc.ts
+++ .umirc.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'dumi'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 import { join } from 'path'
+import { readFileSync } from 'fs'
+
+const globalCss = readFileSync(join(__dirname, 'public', 'global.css'))
 
 export default defineConfig({
     title: 'Fast UI',
@@ -8,9 +11,8 @@ export default defineConfig({
     logo: 'https://avatars.githubusercontent.com/u/91562499?s=200&v=4',
     outputPath: 'docs-dist',
     mode: 'site',
+    styles: [globalCss.toString('utf-8')],
     locales: [['zh-CN', '中文']],
-    // mfsu: {},
-    headScripts: [{ src: '/main.bundle.js', defer: true }],
     devtool: 'eval-source-map',
     targets: false,
     navs: {
@@ -22,37 +24,39 @@ export default defineConfig({
             },
         ],
     },
-    chunks: [],
+    chunks: process.env.NODE_ENV === 'production' ? [] : undefined,
     chainWebpack: (config) => {
-        config.output.filename('[name].bundle.js')
+        if (process.env.NODE_ENV === 'production') {
+            config.output.filename('[name].bundle.js')
 
-        config.plugin('HtmlWebpackPlugin').use(HtmlWebpackPlugin, [
-            {
-                template: join(process.cwd(), 'document.ejs'),
-            },
-        ])
+            config.plugin('HtmlWebpackPlugin').use(HtmlWebpackPlugin, [
+                {
+                    template: join(process.cwd(), 'document.ejs'),
+                },
+            ])
 
-        config.optimization.splitChunks({
-            chunks: 'all',
-            maxSize: 524288,
-            minSize: 262144,
-            minChunks: 1,
-            maxAsyncRequests: 30,
-            maxInitialRequests: 30,
-            enforceSizeThreshold: 50000,
-            cacheGroups: {
-                defaultVendors: {
-                    test: /[\\/]node_modules[\\/]/,
-                    priority: -10,
-                    reuseExistingChunk: true,
+            config.optimization.splitChunks({
+                chunks: 'all',
+                maxSize: 524288,
+                minSize: 262144,
+                minChunks: 1,
+                maxAsyncRequests: 30,
+                maxInitialRequests: 30,
+                enforceSizeThreshold: 50000,
+                cacheGroups: {
+                    defaultVendors: {
+                        test: /[\\/]node_modules[\\/]/,
+                        priority: -10,
+                        reuseExistingChunk: true,
+                    },
+                    default: {
+                        name: 'umi',
+                        minChunks: 2,
+                        priority: -20,
+                        reuseExistingChunk: true,
+                    },
                 },
-                default: {
-                    name: 'umi',
-                    minChunks: 2,
-                    priority: -20,
-                    reuseExistingChunk: true,
-                },
-            },
-        })
+            })
+        }
     },
 })
