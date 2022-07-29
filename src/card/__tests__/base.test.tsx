10e3154dd4963d1d7d8ac2b03f79a4dbb44ce20e
robothot <854363956@qq.com>
2022-07-29 09:42:33 +0800

    feat: 添加测试用例

--- src/card/__tests__/base.test.tsx
+++ src/card/__tests__/base.test.tsx
@@ -1,6 +1,25 @@
 import React from 'react'
 import { render } from '@testing-library/react'
-import TestCard from '../__demo__/base'
+import Card from '..'
+
+const TestCard = () => {
+    return (
+        <>
+            <div
+                style={{
+                    height: 400,
+                    width: 500,
+                }}
+            >
+                <Card title="Default size card" extra={<a href="#">More</a>}>
+                    <p>Card content</p>
+                    <p>Card content</p>
+                    <p>Card content</p>
+                </Card>
+            </div>
+        </>
+    )
+}
 
 test('snapshot', async () => {
     const { container } = render(<TestCard />)
