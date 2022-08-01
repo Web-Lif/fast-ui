d5cc5ca791141b186f99225ca2cf42e0e9983e07
robothot <854363956@qq.com>
2022-08-01 10:32:08 +0800

    feat: 添加 Tabs 按照百分比进行布局

--- src/tabs/index.tsx
+++ src/tabs/index.tsx
@@ -1,1 +1,28 @@
-export { default as Tabs } from 'antd/es/tabs'
+import { css, cx } from '@emotion/css'
+import AntTabs, { TabsProps } from 'antd/es/tabs'
+import React from 'react'
+
+const InternalTabs = ({ className, ...restProps }: TabsProps) => {
+    return (
+        <AntTabs
+            className={cx({
+                [className || '']: typeof className === 'string',
+                [css`
+                    height: 100%;
+                `]: true,
+            })}
+            {...restProps}
+        />
+    )
+}
+
+type InternalTabsType = typeof InternalTabs
+
+interface TabsInterface extends InternalTabsType {
+    TabPane: typeof AntTabs.TabPane
+}
+
+const Tabs = InternalTabs as TabsInterface
+Tabs.TabPane = AntTabs.TabPane
+
+export default Tabs
