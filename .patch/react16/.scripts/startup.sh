644c840fa29f5991e1e030dc4aa5f43eefe7972e
robothot <854363956@qq.com>
2022-11-16 17:42:03 +0800

    fix: 修复发布的时候的问题

--- .patch/react16/.scripts/startup.sh
+++ .patch/react16/.scripts/startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
+oldversion=`grep -E '"version":\s*"[0-9A-Za-z\.]+"' package.json` && version=`echo "${oldversion}" | sed -e 's/",/-react16",/'` && sed -i -e "s/${oldversion}/${version}/" package.json
 sed -i -e 's/"jsx":\s*"react-jsx",/"jsx": "react",/g' tsconfig.json
-
 bash .patch/react16/.scripts/addImportReact.sh
