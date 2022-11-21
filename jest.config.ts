0d4180c761364eacdc3f0e8a216453b6e1dfdbdb
robothot <854363956@qq.com>
2022-11-21 10:07:01 +0800

    fix: 修复 jest 测试的问题

--- jest.config.ts
+++ jest.config.ts
@@ -133,10 +133,10 @@ export default {
     // runner: "jest-runner",
 
     // The paths to modules that run some code to configure or set up the testing environment before each test
-    setupFiles: ['./tests/setup.ts'],
+    // setupFiles: [],
 
     // A list of paths to modules that run some code to configure or set up the testing framework before each test
-    // setupFilesAfterEnv: [],
+    setupFilesAfterEnv: ['./tests/setup.ts'],
 
     // The number of seconds after which a test is considered as slow and reported as such in the results.
     // slowTestThreshold: 5,
