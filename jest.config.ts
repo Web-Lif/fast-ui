d7d32a688b44089f044534efbba1e6c54c18c95f
robothot <854363956@qq.com>
2022-11-21 09:44:02 +0800

    fix: 修复 jest 的依赖问题

--- jest.config.ts
+++ jest.config.ts
@@ -76,21 +76,20 @@ export default {
     // ],
 
     // An array of file extensions your modules use
-    // moduleFileExtensions: [
-    //   "js",
-    //   "mjs",
-    //   "cjs",
-    //   "jsx",
-    //   "ts",
-    //   "tsx",
-    //   "json",
-    //   "node"
-    // ],
+    moduleFileExtensions: [
+        'js',
+        'mjs',
+        'cjs',
+        'jsx',
+        'ts',
+        'tsx',
+        'json',
+        'node',
+    ],
 
     // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-    moduleNameMapper: {
-        '^antd/es/(.*)$': '<rootDir>/node_modules/antd/lib/$1',
-    },
+    // moduleNameMapper: {
+    // },
 
     // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
     // modulePathIgnorePatterns: [],
@@ -178,10 +177,11 @@ export default {
     },
 
     // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-    // transformIgnorePatterns: [
-    //   "/node_modules/",
-    //   "\\.pnp\\.[^\\/]+$"
-    // ],
+    transformIgnorePatterns: [
+        'node_modules/(?!antd)/',
+        //   "/node_modules/",
+        //   "\\.pnp\\.[^\\/]+$"
+    ],
 
     // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
     // unmockedModulePathPatterns: undefined,
